eac0791bbd5c57d7c6dd5ccb9c9a83e9e954f710
Rafael-Correzi <112991986+Rafael-Correzi@users.noreply.github.com>
2024-09-30 14:51:45 -0300

    Add buttons on HTML file and their interactions on JS file

--- Script.js
+++ Script.js
@@ -1,3 +1,5 @@
+
+
 //The computer randomly chooses a move
 
 function getComputerChoice() {
@@ -15,14 +17,8 @@ function getComputerChoice() {
 
 //The player chooses a move
 
-function getPlayerChoice() {
-  let playerSelection = prompt('Choose your move (rock, paper, or scissors)');
-  playerSelection = playerSelection.toLowerCase();
-  if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
-  return playerSelection;
-  }
-  else alert('Invalid move! Choose between "rock", "paper", or "scissors"');
-  getPlayerChoice();
+function getPlayerChoice(choice) {
+  compareMoves(choice, getComputerChoice());
 }
 //A function compares both moves and returns a string with the result
 
@@ -32,7 +28,7 @@ function compareMoves(playerSelection, computerSelection) {
     return(0);
   }
   else if (playerSelection === 'rock' && computerSelection === 'paper') {
-    console.log("You lose! Paper beats Rock");
+    console.log("You lose! Rock gets beaten by paper");
     return(-1);
   }
   else if (playerSelection === 'rock' && computerSelection === 'scissors') {
@@ -40,7 +36,7 @@ function compareMoves(playerSelection, computerSelection) {
     return(1);
   }
   else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-    console.log("You lose! Scissors beats Paper");
+    console.log("You lose! Paper gets beaten by scissors");
     return(-1);
   }
   else if (playerSelection === 'paper' && computerSelection === 'rock') {
@@ -48,43 +44,30 @@ function compareMoves(playerSelection, computerSelection) {
     return(1);
   }
   else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-    console.log("You Lose! Rock beats Scissors");
+    console.log("You Lose! Scissors get beaten by rock");
     return(-1);
   }
   else console.log("You win! Scissors beats Paper");
   return(1);
 }
 
-//A function repeats the game 5 times and keeps a tally of the score, reporting the winner and loser at the end
+//
 
-function game() {
-  let yourScore = 0;
-  let computerScore = 0;
-  let draws = 0;
-  let winner = null;
+const rock = document.querySelector("#rock");
+rock.addEventListener("click", () => {
+    getPlayerChoice('rock');
+});
+const paper = document.querySelector("#paper");
+paper.addEventListener("click", () => {
+  getPlayerChoice('paper');
+});
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", () => {
+  getPlayerChoice('scissors');
+});
 
-  for (i=0; i<5; i++) {
-    let result = compareMoves(getPlayerChoice(), getComputerChoice());
-    if (result === 0) {
-      draws += 1;
-    } 
-    else if (result === -1 ) {
-      computerScore += 1;
-    }
-    else yourScore += 1;   
-  }
 
-  if (yourScore > computerScore) {
-    winner = "You have won the game!";
-  }
-  else if (yourScore < computerScore){
-    winner = "The Computer has won the game!";
-  }
-  else winner = "It's a tie!";
 
-  console.log ("You've won " + yourScore + " times, lost " + computerScore + " times, and drawn " + draws + " times. " + winner );
-}
 
 //Run the game 
 
-game();
