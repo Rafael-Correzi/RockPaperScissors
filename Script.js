b533bcf47874f01c1811a5fffc81b19a1300435a
Rafael-Correzi <112991986+Rafael-Correzi@users.noreply.github.com>
2024-09-30 17:43:24 -0300

    Add score tally for both players and final result message

--- Script.js
+++ Script.js
@@ -1,57 +1,11 @@
 
+const result = document.querySelector("#result");
 
-//The computer randomly chooses a move
+const playerScore = document.querySelector("#player");
 
-function getComputerChoice() {
-  let computerSelection = Math.floor(Math.random()*3) + 1;
-  if (computerSelection == 1 ) {
-    computerSelection = 'rock';
-  }
-  else if (computerSelection == 2) {
-    computerSelection = 'paper';
-  }
-  else computerSelection = 'scissors';
-  console.log(computerSelection);
-  return computerSelection;
-}
+const computerScore = document.querySelector("#computer");
 
-//The player chooses a move
-
-function getPlayerChoice(choice) {
-  compareMoves(choice, getComputerChoice());
-}
-//A function compares both moves and returns a string with the result
-
-function compareMoves(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    console.log("It's a draw!");
-    return(0);
-  }
-  else if (playerSelection === 'rock' && computerSelection === 'paper') {
-    console.log("You lose! Rock gets beaten by paper");
-    return(-1);
-  }
-  else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-    console.log("You win! Rock beats Scissors");
-    return(1);
-  }
-  else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-    console.log("You lose! Paper gets beaten by scissors");
-    return(-1);
-  }
-  else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    console.log("You win! Paper beats Rock");
-    return(1);
-  }
-  else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-    console.log("You Lose! Scissors get beaten by rock");
-    return(-1);
-  }
-  else console.log("You win! Scissors beats Paper");
-  return(1);
-}
-
-//
+const message = document.querySelector("#message");
 
 const rock = document.querySelector("#rock");
 rock.addEventListener("click", () => {
@@ -65,6 +19,75 @@ const scissors = document.querySelector("#scissors");
 scissors.addEventListener("click", () => {
   getPlayerChoice('scissors');
 });
+//The computer randomly chooses a move
+
+function getComputerChoice() {
+  let computerSelection = Math.floor(Math.random()*3) + 1;
+  if (computerSelection == 1 ) {
+    computerSelection = 'rock';
+  }
+  else if (computerSelection == 2) {
+    computerSelection = 'paper';
+  }
+  else computerSelection = 'scissors';
+  return computerSelection;
+}
+
+//The player chooses a move
+
+function getPlayerChoice(choice) {
+  if (scoreC == 5 || scoreP == 5){
+    scoreC = 0;
+    scoreP = 0;
+  }
+  compareMoves(choice, getComputerChoice());
+}
+//A function compares both moves and displays the result on the page
+
+let scoreP = 0;
+let scoreC = 0;
+
+function compareMoves(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    result.textContent = "It's a draw!";
+  }
+  else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    result.textContent = "The computer played " + computerSelection + ". You lose! Rock gets beaten by paper.";
+    scoreC += 1;
+  }
+  else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    result.textContent =  "The computer played " + computerSelection + ". You win! Rock beats Scissors.";
+    scoreP += 1;
+  }
+  else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    result.textContent = "The computer played " + computerSelection + ". You lose! Paper gets beaten by scissors.";
+    scoreC += 1;
+  }
+  else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    result.textContent = "The computer played " + computerSelection + ". You win! Paper beats Rock.";
+    scoreP += 1;
+  }
+  else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    result.textContent =  "The computer played " + computerSelection + ". You Lose! Scissors get beaten by rock.";
+    scoreC += 1;
+  }
+  else { 
+    result.textContent = "The computer played " + computerSelection + ".   You win! Scissors beats Paper.  ";
+    scoreP += 1
+  }
+  playerScore.textContent= scoreP;
+  computerScore.textContent = scoreC;
+  if (scoreP == 5) {
+    message.textContent = ". You won this match!"
+  }
+  if (scoreC == 5) {
+    message.textContent = ". You lost this match"
+  }
+}
+
+//
+
+
 
 
 
